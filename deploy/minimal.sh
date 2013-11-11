67f6efbcc0efd1dd2c7879dff2dc7a737e232137
Alexander Frey <sashafrey@gmail.com>
2013-11-11 19:34:09 +0000

    minimal deployment script

---
+++ deploy/minimal.sh
@@ -0,0 +1,6 @@
+sudo apt-get install git -q -y
+
+git config --global user.name "Alexander Frey"
+git config --global user.email "sashafrey@gmail.com"
+
+git clone https://github.com/sashafrey/topicmod.git

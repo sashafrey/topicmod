04c645b56a73f593864b03760f965ac4487d1ce3
Alexander Frey <sashafrey@gmail.com>
2013-11-11 22:04:55 +0000

    transfer deploy scripts to separate repository

--- deploy/storage_server.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update -q -y
-sudo apt-get install nfs-kernel-server nfs-common -q -y
-sudo mkdir /deploy
-sudo sh -c 'echo "/deploy *(ro,nohide)" >> /etc/exports'
-sudo service nfs-kernel-server restart

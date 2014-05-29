cd9b37f23d4dca5ffdf92f96be4a7345ad459809
Alexander Frey <sashafrey@gmail.com>
2014-05-29 11:42:59 +0200

    Add missing dll_main.cpp

---
+++ src/artm/dll_main.cpp
@@ -0,0 +1,31 @@
+#if defined(_WIN32) || defined(_WIN64)
+#include "glog/logging.h"
+
+#include <windows.h>
+
+BOOL APIENTRY DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved ) {
+	switch ( ul_reason_for_call )	{
+		case DLL_PROCESS_ATTACH:
+      // A process is loading the DLL.
+      FLAGS_log_dir = ".";
+      FLAGS_logbufsecs = 0;
+      ::google::InitGoogleLogging(".");
+  		break;
+
+    case DLL_THREAD_ATTACH:
+		  // A process is creating a new thread.
+		  break;
+
+    case DLL_THREAD_DETACH:
+		  // A thread exits normally.
+		  break;
+		
+    case DLL_PROCESS_DETACH:
+		  // A process unloads the DLL.
+		  break;
+	}
+
+	return TRUE;
+}
+
+#endif

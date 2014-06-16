304259f160a345310ffa48033a764828ecd905b4
Andrey Shadrikov <notvuvko@gmail.com>
2014-06-17 00:37:17 +0400

    Fixed compiler flags. Some fix with FindZeroMQ module but still need more to do with it.

--- 3rdparty/rpcz/cmake_modules/FindZeroMQ.cmake
+++ 3rdparty/rpcz/cmake_modules/FindZeroMQ.cmake
@@ -7,32 +7,34 @@
 
 include(LibFindMacros)
 
+set(ZEROMQ_ROOT "" CACHE PATH "Path to ZeroMQ directory.")
+
 IF (UNIX)
-	# Use pkg-config to get hints about paths
-	libfind_pkg_check_modules(ZeroMQ_PKGCONF libzmq)
+    # Use pkg-config to get hints about paths
+    libfind_pkg_check_modules(ZeroMQ_PKGCONF libzmq)
 
-	# Include dir
-	find_path(ZeroMQ_INCLUDE_DIR
-	  NAMES zmq.hpp
-	  PATHS ${ZEROMQ_ROOT}/include ${ZeroMQ_PKGCONF_INCLUDE_DIRS}
-	)
+    # Include dir
+    find_path(ZeroMQ_INCLUDE_DIR
+      NAMES zmq.hpp
+      PATHS ${ZEROMQ_ROOT}/include ${ZeroMQ_PKGCONF_INCLUDE_DIRS}
+    )
 
-	# Finally the library itself
-	find_library(ZeroMQ_LIBRARY
-	  NAMES zmq
-	  PATHS ${ZEROMQ_ROOT}/lib ${ZeroMQ_PKGCONF_LIBRARY_DIRS}
-	)
-ELSEIF (WIN32)
-	find_path(ZeroMQ_INCLUDE_DIR
-	  NAMES zmq.hpp
-	  PATHS ${ZEROMQ_ROOT}/include ${CMAKE_INCLUDE_PATH}
-	)
-	# Finally the library itself
-	find_library(ZeroMQ_LIBRARY
-	  NAMES libzmq
-	  PATHS ${ZEROMQ_ROOT}/lib ${CMAKE_LIB_PATH}
-	)
-ENDIF()
+    # Finally the library itself
+    find_library(ZeroMQ_LIBRARY
+      NAMES zmq
+      PATHS ${ZEROMQ_ROOT}/lib ${ZeroMQ_PKGCONF_LIBRARY_DIRS}
+    )
+ELSE (UNIX)
+    find_path(ZeroMQ_INCLUDE_DIR
+      NAMES zmq.h
+      PATHS ${ZEROMQ_ROOT}/include ${CMAKE_INCLUDE_PATH}
+    )
+    # Finally the library itself
+    find_library(ZeroMQ_LIBRARY
+      NAMES libzmq#-v110-mt-4_0_4.lib
+      PATHS ${ZEROMQ_ROOT}/lib ${CMAKE_LIB_PATH}
+    )
+ENDIF(UNIX)
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this this lib depends on.

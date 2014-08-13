7a370fd9091262f74c12efc3e6196574f0eb9a9f
Alexander Frey <sashafrey@gmail.com>
2014-08-14 00:37:55 +0200

    Update documentation (devguide) with CMake instructions

--- libs/README.txt
+++
@@ -1,3 +0,0 @@
-Download libs from here:
-https://s3-eu-west-1.amazonaws.com/artm/libs_win32_v100.7z --- for Visual Studio 2010 SP4
-https://s3-eu-west-1.amazonaws.com/artm/libs_win32_v110.7z --- for Visual Studio 2012
---
+++ utils/gen_proto.bat
@@ -0,0 +1,11 @@
+# Procedure to generate protobuf files:
+# 1. Copy the following files into $(BIGARTM_ROOT)/src/
+#    - this script
+#    - $(BIGARTM_ROOT)/build/bin/CONFIG/protoc.exe
+#    - $(BIGARTM_ROOT)/build/bin/CONFIG/protoc-gen-cpp_rpcz.exe
+#    Here CONFIG can be either Debug or Release (both options will work equally well).
+# 2. cd $(BIGARTM_ROOT)/src/
+# 3. run this script.
+
+.\protoc.exe --cpp_out=. --python_out=. .\artm\messages.proto
+.\protoc.exe --cpp_out=. --rpcz_plugin_out=. .\artm\core\internals.proto
